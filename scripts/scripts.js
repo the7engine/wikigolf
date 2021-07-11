076e4a8e4a142d203285622cb860b27310820ec1
the7engine <toby.cameron1@gmail.com>
2021-07-11 19:49:41 +1000

    Driving Range updated with random target provided

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+const topics = [
+    {title:"Citizen Kane", url:"https://en.wikipedia.org/wiki/Citizen_Kane"},
+    {title:"Moon Landing", url:"https://en.wikipedia.org/wiki/Moon_landing"},
+    {title:"Golf", url:"https://en.wikipedia.org/wiki/Golf"},
+    {title:"Harold Holt", url:"https://en.wikipedia.org/wiki/Harold_Holt"},
+    {title:"First Fleet", url:"https://en.wikipedia.org/wiki/First_Fleet"},
+    {title:"Penicillin", url:"https://en.wikipedia.org/wiki/Penicillin"},
+    {title:"The Rumble in the Jungle", url:"https://en.wikipedia.org/wiki/The_Rumble_in_the_Jungle"},
+    {title:"Thriller (Album)", url:"https://en.wikipedia.org/wiki/Thriller_(album)"}
+];
+var x = Math.floor(Math.random() * topics.length);
+
+console.log(topics[x].title);
+window.onload = function(){
+document.getElementById('target').innerHTML = topics[x].title + ' @ ' + topics[x].url;
+}
